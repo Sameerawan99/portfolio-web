e19f8423f9116e7d2be38aa6fdf919db824fadd8
Sameerawan99 <165399018+Sameerawan99@users.noreply.github.com>
2024-08-25 22:16:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+if (typeof Typed !== 'undefined') {
+    var typed = new Typed(".auto-type", {
+        strings: ["Front-End Web Developer", "Engineer","Student"],
+        typeSpeed: 90,
+        backSpeed: 80,
+        loop: true
+    });
+} else {
+    console.error('Typed.js library failed to load');
+}
+
+
+
